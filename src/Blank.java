eb3c7da5af3780018c6c911e890e6c0c1a7b138f
ibz1330 <ibz0502291330@gmail.com>
2024-11-12 19:12:14 +0200

    end of day

---
+++ src/Blank.java
@@ -0,0 +1,61 @@
+public class Blank {
+
+//    public void update() {
+//        int num = 1;
+//        int nextX = x, nextY = y;
+//        if (keyH.upPressed && y > 2) {
+//            direction = "up";
+//            System.out.println(direction);
+//            this.currentY = (y)/titleSize;
+//            this.currentX = (x + speed) /titleSize;
+//            System.out.println("tempY: "+ currentY +", "+"tempX: "+currentX);
+//            boolean bool = is2(num,arr,currentY,currentX);
+//            System.out.println(bool);
+//            if (bool) y -= speed;
+//            System.out.println("x: " + x + ", " + "y: " + y);
+//            System.out.println("---------------------------");
+//        }
+//        if (keyH.downPressed && y < 576 - titleSize - 2) {
+//            direction = "down";
+//            System.out.println(direction);
+//            this.currentY = (y +speed+titleSize) / titleSize;
+//            this.currentX = (x +speed) /titleSize;
+//            System.out.println("tempY: "+ currentY +", "+"tempX: "+currentX);
+//            boolean bool = is2(num,arr,currentY,currentX);
+//            System.out.println(bool);
+//            if (bool) y += speed;
+//            System.out.println("x: " + x + ", " + "y: " + y);
+//            System.out.println("---------------------------");
+//        }
+//        if (keyH.leftPressed && x > 2) {
+//            direction = "left";
+//            System.out.println(direction);
+//            currentY = (y+speed+titleSize)/titleSize;
+//            currentX = (x-titleSize)/titleSize;
+//            System.out.println("tempY: "+ currentY +", "+"tempX: "+currentX);
+//            boolean bool = is2(num,arr,currentY,currentX);
+//            System.out.println(bool);
+//            if (bool) x -= speed;
+//            System.out.println("x: " + x + ", " + "y: " + y);
+//            System.out.println("---------------------------");
+//        }
+//        if (keyH.rightPressed && x < 768 - titleSize - 2) {
+//            System.out.println(direction);
+//            direction = "right";
+//            currentY = (y) / titleSize;
+//            currentX = (x +  titleSize )/titleSize;
+//            System.out.println("tempY: "+ currentY +", "+"tempX: "+currentX);
+//            boolean bool = is2(num,arr,currentY,currentX);
+//            System.out.println(bool);
+//            if (bool) x += speed;
+//            System.out.println("x: " + x + ", " + "y: " + y);
+//            System.out.println("---------------------------");
+//
+//        }
+//
+//
+//
+//    public static void main(String[] args) {
+//
+//    }
+}
